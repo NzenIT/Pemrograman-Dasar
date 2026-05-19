432a90d7b17267268d30dc697e919f2156440717
nzenatp <best.nayaka@gmail.com>
2026-05-19 18:14:52 +0700

    Initial commit

--- Praktikum/Fungsiarray.java
+++ Praktikum/Fungsiarray.java
@@ -1,22 +1,7 @@
 import java.util.Scanner;
 public class Fungsiarray {
- 
 
-    // MEmbuat Fungsi mencari nilai terbesar 
-    static int getNilaiMax(int[] arr) {
-
-        int max = arr[0];
-
-        for (int i = 1; i < arr.length; i++) {
-
-            if (arr[i] > max) {
-                max = arr[i];
-            }
-        }
-
-        return max;
-    }
-
+    
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -39,6 +24,22 @@ public class Fungsiarray {
         System.out.println("Nilai terbesar adalah: " + hasil);
 
         sc.close();
+    }
+
+
+        // MEmbuat Fungsi mencari nilai terbesar 
+    static int getNilaiMax(int[] arr) {
+
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        return max;
     }
 }
 
